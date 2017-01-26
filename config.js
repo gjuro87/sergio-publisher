4de8836de2ad0434c8200e848cd10d38ab46feba
Jurica Grgicevic <jurica.grgicevic@socialbakers.com>
2017-01-26 21:20:09 +0100

    config file

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	page: 'cat.from.tice.sergio',
+	tokenFile: 'token.js',
+	client_id: '',
+	client_secret: '',
+	scope: 'email, user_about_me, manage_pages, publish_actions, publish_pages',
+	redirect_uri: 'http://localhost:3000/auth/facebook',
+	location: 'Stanići, Croatia',
+	degree_type: 'C',
+	WEATHER_MESSAGE: 'Mjauuu, ledeno u p.... materinu!, mjau.',
+	WIND_MESSAGE: 'Mjauuu, mjauuu, otpuhalo meeee.',
+	HUMIDITY_MESSAGE: 'mjauuu, mjauuu, umrit cu od vlage.',
+	DEFAULT_MESSAGE: 'Mjau, mjauuuu, mjauu!',
+}
